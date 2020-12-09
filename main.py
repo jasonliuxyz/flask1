273e67bc3d6f08070300492ad94e9f79a161f5f6
root <jason.liu>
2020-12-09 13:40:12 +0800

    使用jinja模板分离业务逻辑和表现逻辑

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import feedparser
 
 app = Flask(__name__)
@@ -12,19 +12,8 @@ def get_news(publication='zhihu'): # 注意这里一定要有默认参数
 	# 通过feedparser获取RSS消息
 	feed = feedparser.parse(RSS_FEED[publication])
 	first_content = feed['entries'][0]  
-	
-	# 返回模板字符串
-	html_format = """
-	<html> <body>
-	<h1> Zhihu Headlines </h1>
-        <b> {0} </b> <br/>
-        <i> {1} </i> <br/>
-        <p> {2} </p> <br/>
-    	<body> </html>"""
-	
-	return html_format.format(first_content.get('title'),
-				first_content.get('published'),
-				first_content.get('summary'))
+
+	return render_template('home.html', title=first_content.get('title'), published=first_content.get('published'), summary=first_content.get('summary'))	
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80, debug=True)
